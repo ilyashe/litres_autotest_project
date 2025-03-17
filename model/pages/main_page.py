8ccddf2c94aca9c829872c3cd2dabef7a7964168
ilyashe <ilya.shebanoff@gmail.com>
2025-03-17 21:40:55 +0200

    Change allure report

--- model/pages/main_page.py
+++ model/pages/main_page.py
@@ -3,49 +3,49 @@ import allure
 
 
 class MainPage:
-    @allure.step("Открытие главной страницы")
     def open(self):
-        browser.open("")
-        return self
+        with allure.step("Открытие главной страницы"):
+            browser.open("")
+            return self
 
-    @allure.step("Ввод имейла")
     def fill_email(self, user):
-        browser.element('[data-testid=tab-login]').click()
-        browser.element('[data-testid=auth__input--enterEmailOrLogin]').type(user.email)
-        browser.element('[data-testid=auth__button--continue]').click()
-        return self
+        with allure.step("Ввод имейла"):
+            browser.element('[data-testid=tab-login]').click()
+            browser.element('[data-testid=auth__input--enterEmailOrLogin]').type(user.email)
+            browser.element('[data-testid=auth__button--continue]').click()
+            return self
 
-    @allure.step("Ввод пароля")
     def fill_password(self, user):
-        browser.element('[data-testid=auth__input--enterPassword]').type(user.password)
-        browser.element('[data-testid=auth__button--enter]').click()
-        return self
+        with allure.step("Ввод пароля"):
+            browser.element('[data-testid=auth__input--enterPassword]').type(user.password)
+            browser.element('[data-testid=auth__button--enter]').click()
+            return self
 
-    @allure.step("Открытие страницы профиля")
     def open_profile(self):
-        browser.element('[data-testid=user-button]').click()
-        return self
+        with allure.step("Открытие страницы профиля"):
+            browser.element('[data-testid=user-button]').click()
+            return self
 
-    @allure.step("Проверка, что юзер авторизован")
     def user_should_be_authorized(self, user):
-        browser.element('[name=first_name]').should(have.value(user.first_name))
-        browser.element('[name=last_name]').should(have.value(user.last_name))
-        return self
+        with allure.step("Проверка, что юзер авторизован"):
+            browser.element('[name=first_name]').should(have.value(user.first_name))
+            browser.element('[name=last_name]').should(have.value(user.last_name))
+            return self
 
-    @allure.step("Проверка, что юзер не авторизован")
     def user_should_not_be_authorized(self):
-        (browser.element('[data-testid=textbox--input__error]').
-         should(have.exact_text('Неверное сочетание логина и пароля')))
-        return self
+        with allure.step("Проверка, что юзер не авторизован"):
+            (browser.element('[data-testid=textbox--input__error]').
+             should(have.exact_text('Неверное сочетание логина и пароля')))
+            return self
 
-    @allure.step("Проверка, что юзер может быть зарегистрирован")
     def user_can_be_registered(self):
-        browser.element('[data-testid=authorization-popup]').should(have.text('Адрес свободен для регистрации'))
-        return self
+        with allure.step("Проверка, что юзер может быть зарегистрирован"):
+            browser.element('[data-testid=authorization-popup]').should(have.text('Адрес свободен для регистрации'))
+            return self
 
-    @allure.step("Проверка, что юзер разлогинен")
     def user_should_be_unauthorized(self):
-        browser.element('[data-testid=tab-login]').should(have.text('Войти'))
-        return self
+        with allure.step("Проверка, что юзер разлогинен"):
+            browser.element('[data-testid=tab-login]').should(have.text('Войти'))
+            return self
 
 main_page = MainPage()
