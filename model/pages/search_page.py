f89eaec00b87fed640b099e07eda767acbb98966
ilyashe <ilya.shebanoff@gmail.com>
2025-03-18 11:29:28 +0200

    Add test_book_search.py

--- model/pages/search_page.py
+++ model/pages/search_page.py
@@ -9,8 +9,13 @@ class SearchPage:
             return self
 
     def should_find_book_with_title(self, book):
-        with allure.step('Открытие редактирования профиля'):
+        with allure.step('Проверка, что книга нашлась'):
             browser.all('[data-testid=art__title]').first.should(have.text(book.title))
+            return self
+
+    def should_find_empty_result(self):
+        with allure.step('Проверка, что нет результатов'):
+            browser.element('[data-testid=search-title__wrapper]').should(have.text('ничего не найдено'))
             return self
 
 search_page = SearchPage()
