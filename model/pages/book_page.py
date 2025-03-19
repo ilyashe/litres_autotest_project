b0ab4ba951a347a3b98a38216ea22d4ad6a70ef6
ilyashe <ilya.shebanoff@gmail.com>
2025-03-19 23:47:34 +0200

    Change book_page click on add book to basket

--- model/pages/book_page.py
+++ model/pages/book_page.py
@@ -1,4 +1,4 @@
-from selene import browser, have, be
+from selene import browser, have, be, command
 import allure
 
 
@@ -6,10 +6,10 @@ class BookPage:
     def add_book_to_basket(self):
         with allure.step('Добавление книги в корзину'):
             if browser.element('[data-testid=book__addToCartButton]').matching(be.visible):
-                browser.element('[data-testid=book__addToCartButton]').click()
+                browser.element('[data-testid=book__addToCartButton]').perform(command.js.click)
             else:
                 browser.element('[data-testid="book-sale-block__PPD--wrapper"]').click()
-                browser.element('[data-testid=book__addToCartButton]').click()
+                browser.element('[data-testid=book__addToCartButton]').perform(command.js.click)
             return self
 
     def should_book_with_price(self, book):
