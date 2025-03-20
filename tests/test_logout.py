d45bdc0c4aa066fadb73d9744997af675e4083a5
ilyashe <ilya.shebanoff@gmail.com>
2025-03-20 15:30:00 +0200

    Change logout test

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -26,6 +26,7 @@ def test_logout():
     main_page.fill_password(user)
     main_page.open_profile()
     main_page.close_modal()
+    main_page.close_modal()
     profile_page.logout()
 
     #THEN
