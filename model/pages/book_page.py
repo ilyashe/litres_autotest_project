16ea5b226dc639c4cee2cd1459dd7f995d8430b6
ilyashe <ilya.shebanoff@gmail.com>
2025-03-20 22:14:51 +0200

    Change add_book_to_basket

--- model/pages/book_page.py
+++ model/pages/book_page.py
@@ -1,15 +1,25 @@
-from selene import browser, have, be
+from selene import browser, have, be, command
 import allure
 
 
 class BookPage:
     def add_book_to_basket(self):
         with allure.step('Добавление книги в корзину'):
+            browser.driver.refresh()
             if browser.element('[data-testid=book__addToCartButton]').with_(timeout=7).matching(be.present):
                 browser.element('[data-testid=book__addToCartButton]').should(be.clickable).click()
             else:
                 browser.element('[data-testid=book-sale-block__PPD--wrapper]').should(be.clickable).click()
                 browser.element('[data-testid=book__addToCartButton]').should(be.clickable).click()
+
+
+            # if browser.element('[data-testid=book__addToCartButton]').with_(timeout=7).matching(be.present):
+            #     while not browser.element('[data-testid="book__goToCartButton"]').matching(be.present):
+            #         browser.element('[data-testid=book__addToCartButton]').should(be.clickable).click()
+            # else:
+            #     browser.element('[data-testid=book-sale-block__PPD--wrapper]').should(be.clickable).click()
+            #     while not browser.element('[data-testid="book__goToCartButton"]').matching(be.present):
+            #         browser.element('[data-testid=book__addToCartButton]').should(be.clickable).click()
             return self
 
     def should_book_with_price(self, book):
