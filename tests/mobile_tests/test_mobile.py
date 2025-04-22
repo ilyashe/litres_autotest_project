c6c016e17dc85f57fccda9cf5c2098e7f167dbb5
ilyashe <ilya.shebanoff@gmail.com>
2025-04-22 22:44:19 +0300

    change tests for bstack

--- tests/mobile_tests/test_mobile.py
+++ tests/mobile_tests/test_mobile.py
@@ -3,15 +3,19 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, be, have
 
 
-
 def test_search_book(android_management):
 
-    with ((step('Прокрутить список языков до RUSSIAN'))):
+    with step('Проверка, что юзер авторизован'):
         browser.element((AppiumBy.ID, 'ru.litres.android:id/choosebutton')).should(be.visible)
         browser.driver.swipe(550, 1350, 550, 1250)
         browser.element((AppiumBy.ID, 'ru.litres.android:id/choosebutton')).click()
         browser.element((AppiumBy.ID, 'com.android.permissioncontroller:id/permission_deny_button')).click()
-        browser.element((AppiumBy.ID, 'ru.litres.android:id/circleButtonSubscriptionPaywallClose')).click()
+
+        if browser.element((AppiumBy.ID, 'ru.litres.android:id/circleButtonSubscriptionPaywallClose')
+                       ).matching(be.present):
+            browser.element((AppiumBy.ID, 'ru.litres.android:id/circleButtonSubscriptionPaywallClose')
+                        ).click()
+        # browser.element((AppiumBy.ID, 'ru.litres.android:id/circleButtonSubscriptionPaywallClose')).click()
         browser.element((AppiumBy.ID, 'ru.litres.android:id/search')).click()
         browser.element((AppiumBy.ID, 'ru.litres.android:id/et_search_query')
                         ).type('Дмитрий Данилов Пустые поезда 2022 года')
